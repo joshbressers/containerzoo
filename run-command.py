1d9ae13052f851ee203970b443bd9706886eb76b
Josh Bressers <josh@bress.net>
2017-10-19 10:43:18 +0200

    Add the ability to look for stdin data

--- run-command.py
+++ run-command.py
@@ -3,16 +3,31 @@
 import sys
 import os
 import subprocess
+import select
+
+stdin_data = False
+
+if select.select([sys.stdin,],[],[],0.0)[0]:
+    stdin_data = True
 
 homedir=os.path.join(os.getenv("HOME"), 'docker-home:/home/container')
 
 the_command = os.path.basename(sys.argv[0])
-print(the_command)
 
 # docker run --rm -i -v $HOME/docker-home:/home/container -u container -t linux-commands $command $@
 #
 
-cmd = ['docker', 'run', '--rm', '-i', '-v', homedir, '-u', 'container', '-t',
-'linux-commands', the_command]
+#cmd = ['docker', 'run', '--rm', '-i', '-v', homedir, '-u', 'container', '-t', 'linux-commands', the_command]
+
+
+# XXX: I'm unsure if we can do this nicely with docker, if we claim a tty,
+# we can't pipe from stdin
+if stdin_data:
+    # We have data waiting on stdin
+    cmd = ['docker', 'run', '--rm', '-a', 'stdin', '-a', 'stdout', '-a', 'stderr', '-i', '-v', homedir, '-u', 'container', 'linux-commands', the_command]
+else:
+    # Nothing is waiting on stdin, we can grab a terminal
+    cmd = ['docker', 'run', '--rm', '-a', 'stdin', '-a', 'stdout', '-a', 'stderr', '-i', '-v', homedir, '-u', 'container', '-t', 'linux-commands', the_command]
+
 cmd.extend(sys.argv[1:])
 output = subprocess.run(cmd)
