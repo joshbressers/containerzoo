b43b08fb08b290fac21030ff486355afaf5d4c09
Josh Bressers <josh@bress.net>
2017-10-19 17:58:44 +0200

    Add trivial file mounting

--- run-command.py
+++ run-command.py
@@ -25,4 +25,20 @@ else:
     cmd = ['docker', 'run', '--rm', '-a', 'stdin', '-a', 'stdout', '-a', 'stderr', '-i', '-v', homedir, '-u', 'container', '-t', 'linux-commands', the_command]
 
 cmd.extend(sys.argv[1:])
+
+# Let's look if the last command is a file or directory
+# XXX: Not every command will do this, for now let's guess
+
+full_path = None
+if len(sys.argv[1:]) > 0:
+    if os.path.exists(sys.argv[-1]):
+        full_path = os.path.abspath(sys.argv[-1])
+
+        if os.path.islink(full_path):
+            # symlinks are hard, let's give up
+            pass
+        else:
+            cmd.insert(2, "-v")
+            cmd.insert(3, "%s:%s" % (full_path, full_path))
+
 os.execve('/usr/local/bin/docker', cmd, os.environ)
